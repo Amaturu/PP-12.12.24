381d734474e7b46c5038e91e0887198805b9bc99
Zhelnovach Andrew <hrthefreeboy@gmail.com>
2024-12-12 17:47:15 +0300

    Deleted unwanted files

--- countcomments.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = 'https://py4e-data.dr-chuck.net/comments_42.html'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-comments = soup.find_all('span', {'class': 'comments'})
-
-print(sum(list(int(tag.contents[0]) for tag in comments)))
